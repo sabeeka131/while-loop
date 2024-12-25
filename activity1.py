f541b241a7c5a902a688da5b5997a5afd3f6c278
Aliza <59551300+sabeeka131@users.noreply.github.com>
2024-12-25 18:10:07 +0500

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+#Input the value of terms
+n = int(input("Enter the value of terms: "))
+
+
+sum = 0 #initialize
+i = 1 #initialize
+while i<=n: #loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+
+
+print("\nSum =", sum)
